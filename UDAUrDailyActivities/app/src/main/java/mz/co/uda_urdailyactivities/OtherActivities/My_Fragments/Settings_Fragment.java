b25e9bdb045ad891a64c9a7e931f6446af31330b
Khalidy Ibraimo Safar <88678823+Khalidy6@users.noreply.github.com>
2023-01-29 19:59:08 +0200

    Add files via upload

---
+++ UDAUrDailyActivities/app/src/main/java/mz/co/uda_urdailyactivities/OtherActivities/My_Fragments/Settings_Fragment.java
@@ -0,0 +1,22 @@
+package mz.co.uda_urdailyactivities.OtherActivities.My_Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mz.co.uda_urdailyactivities.R;
+
+
+public class Settings_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
